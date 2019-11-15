48afb37da0e96df360ecb4ce2fdcc58d7728f9c0
Rasel Ahmed <rsl.ahmed383@gmail.com>
2019-11-15 22:22:59 +0600

    'changed3'

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,70 @@
     //   nice select
     $('select').niceSelect();
 
-    
+     // Testomonial Slider
+     $('.testomonial-slider-active').slick({
+      dots: false,
+      infinite: true,
+      arrows: false,
+      autoplay: false,
+      fade: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [{
+          breakpoint: 1500,
+          settings: {
+            arrows: false,
+            dots: false,
+            infinite: true,
+            autoplay: false,
+            fade: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },{
+          breakpoint: 1350,
+          settings: {
+            arrows: false,
+            dots: false,
+            infinite: true,
+            autoplay: false,
+            fade: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },{
+          breakpoint: 992,
+          settings: {
+            arrows: false,
+            dots: false,
+            infinite: true,
+            autoplay: false,
+            fade: false,
+            speed: 300,
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },{
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            dots: false,
+            infinite: true,
+            autoplay: false,
+            fade: false,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
 
 
 
