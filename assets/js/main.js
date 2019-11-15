e35ff81ee7674d566f7244522f13945a8a906355
Rasel Ahmed <rsl.ahmed383@gmail.com>
2019-11-16 01:53:17 +0600

    changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,18 +39,6 @@
             slidesToScroll: 1
           }
         },{
-          breakpoint: 1350,
-          settings: {
-            arrows: false,
-            dots: false,
-            infinite: true,
-            autoplay: false,
-            fade: false,
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        },{
           breakpoint: 992,
           settings: {
             arrows: false,
@@ -80,6 +68,71 @@
         // instead of a settings object
       ]
     });
+
+
+     // hire Slider
+     $('.hire-p-slider').slick({
+      dots: false,
+      infinite: true,
+      arrows: false,
+      autoplay: false,
+      fade: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [{
+          breakpoint: 1500,
+          settings: {
+            arrows: false,
+            dots: false,
+            infinite: true,
+            autoplay: false,
+            fade: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },{
+          breakpoint: 1200,
+          settings: {
+            arrows: false,
+            dots: false,
+            infinite: true,
+            autoplay: false,
+            fade: false,
+            speed: 300,
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },{
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            dots: false,
+            infinite: true,
+            autoplay: false,
+            fade: false,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+
+    setTimeout(function(){
+      $('.rp-wrapper').css({"opacity": "1", "visibility": "visible"});
+      $('.register-pupup').css("transform", "scale(1)");
+   },5000);
+
+   $(".cls-btn").click(function(){
+    $('.rp-wrapper').css("display", "none");
+    event.preventDefault();
+  });
 
 
 
